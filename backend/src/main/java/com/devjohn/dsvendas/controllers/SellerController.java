8e88717b159211ca3a3e3312d89956990c7157ea
João Pedro <joaogoes1337@gmail.com>
2021-09-16 21:38:06 -0300

    Layers

---
+++ backend/src/main/java/com/devjohn/dsvendas/controllers/SellerController.java
@@ -0,0 +1,26 @@
+package com.devjohn.dsvendas.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devjohn.dsvendas.dto.SellerDTO;
+import com.devjohn.dsvendas.services.SellerService;
+
+@RestController
+@RequestMapping(value = "/sellers")
+public class SellerController {
+
+	@Autowired
+	private SellerService service;
+	
+	@GetMapping
+	public ResponseEntity<List<SellerDTO>> findAll() {
+	List<SellerDTO> list = service.findAll();
+	return ResponseEntity.ok(list);
+	}
+}
---
+++ backend/src/main/java/com/devjohn/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.devjohn.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devjohn.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
